54ee1c50cf7f877a51bb8fcd05c6d255486e63e6
Jeremy Wang <jeremywang512@gmail.com>
2023-03-28 13:24:04 +0800

    Added some TODO

--- Calculator.kt
+++ Calculator.kt
@@ -1,1 +1,23 @@
-// Hello
+// TODO: SSS Calculator
+fun SSSCalc() {
+
+}
+
+// TODO: Pag-Ibig Calculator
+fun PagIbigCalc() {
+
+}
+
+// TODO: PhilHealth Calculator
+fun PhilHealthCalc() {
+
+}
+
+// TODO: Tax Calculator
+fun TaxCalc() {
+
+}
+
+fun main(args: Array<String>) {
+    
+}
