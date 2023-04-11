10b74b23dadd4fb49bbd8934da9efe5927511eae
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2023-04-11 09:16:36 +0800

    Fixed double frame render

--- src/main/kotlin/CalculatorController.kt
+++ src/main/kotlin/CalculatorController.kt
@@ -1,36 +1,31 @@
 class CalculatorController(calculatorView: CalculatorView, calculatorModel: CalculatorModel)
 {
-    // Instantiate classes
-    val model = CalculatorModel()
-    val view = CalculatorView()
-
     init {
         // Add an event listener to the calculate button
-        this.view.setCalculateBtnListener() {
+        calculatorView.setCalculateBtnListener {
             // Get the salary from the text field
-            val salary = view.getSalaryTF()
+            val salary = calculatorView.getSalaryTF()
 
             // If the salary is not null, calculate the SSS, Pag-Ibig, and PhilHealth contributions
-            if (salary != null) {
-                val incomeTax = model.computeIncomeTax(salary)
-                val netPayAfterTax = model.computeNetIncome(salary)
-                val sss = model.computeSSS(salary)
-                val philHealth = model.computePhilHealth(salary)
-                val pagIbig = model.computePagIbig(salary)
-                val totalContrib = model.computeContribution(salary)
-                val totalDedux = model.computeTotalDeductions(salary)
-                val netPayAfterDedux = salary - totalDedux
+            val incomeTax = calculatorModel.computeIncomeTax(salary)
+            val netPayAfterTax = calculatorModel.computeNetIncome(salary)
+            val sss = calculatorModel.computeSSS(salary)
+            val philHealth = calculatorModel.computePhilHealth(salary)
+            val pagIbig = calculatorModel.computePagIbig(salary)
+            val totalContrib = calculatorModel.computeContribution(salary)
+            val totalDedux = calculatorModel.computeTotalDeductions(salary)
+            val netPayAfterDedux = salary - totalDedux
 
-                // Set the text of the output text fields to the calculated values
-                view.setIncomeTaxTF(String.format("%.2f", incomeTax))
-                view.setNetPayAfterTax(String.format("%.2f", netPayAfterTax))
-                view.setSSSTF(String.format("%.2f", sss))
-                view.setPhilHealthTF(String.format("%.2f", philHealth))
-                view.setPagIbigTF(String.format("%.2f", pagIbig))
-                view.setTotalContribTF(String.format("%.2f", totalContrib))
-                view.setTotalDeduxTF(String.format("%.2f", totalDedux))
-                view.setNetPayAfterDedux(String.format("%.2f", netPayAfterDedux))
-            }
+            // Set the text of the output text fields to the calculated values
+            calculatorView.setIncomeTaxTF(String.format("%.2f", incomeTax))
+            calculatorView.setNetPayAfterTax(String.format("%.2f", netPayAfterTax))
+            calculatorView.setSSSTF(String.format("%.2f", sss))
+            calculatorView.setPhilHealthTF(String.format("%.2f", philHealth))
+            calculatorView.setPagIbigTF(String.format("%.2f", pagIbig))
+            calculatorView.setTotalContribTF(String.format("%.2f", totalContrib))
+            calculatorView.setTotalDeduxTF(String.format("%.2f", totalDedux))
+            calculatorView.setNetPayAfterDedux(String.format("%.2f", netPayAfterDedux))
+            calculatorView.displayPieChart()
         }
     }
 }
